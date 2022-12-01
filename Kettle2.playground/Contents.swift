7cd54243f1909808123a37edae5650d830d84836
Yuta Seto <setoon47@yahoo.co.jp>
2022-12-01 23:54:59 +0900

    startメソッドの追加、水量の加減と上限の設定、水量によってかかる時間の変化の追加

--- Kettle2.playground/Contents.swift
+++ Kettle2.playground/Contents.swift
@@ -15,8 +15,23 @@ var waterAmount: Int = 0 //0<y<=1200 ml
 
 class Alarm {
     var timer: Timer?
+    
     func start() {
-        timer = Timer.scheduledTimer(timeInterval: 1,target: self,selector: #selector(countup),userInfo: nil,repeats: true )
+        if waterAmount > 0 && waterAmount <= 200{
+            timer = Timer.scheduledTimer(timeInterval: 1,target: self,selector: #selector(countup),userInfo: nil,repeats: true)
+        }else if waterAmount > 200 && waterAmount <= 400{
+            timer = Timer.scheduledTimer(timeInterval: 2,target: self,selector: #selector(countup),userInfo: nil,repeats: true)
+        }else if waterAmount > 400 && waterAmount <= 600{
+            timer = Timer.scheduledTimer(timeInterval: 3,target: self,selector: #selector(countup),userInfo: nil,repeats: true)
+        }else if waterAmount > 600 && waterAmount <= 800{
+            timer = Timer.scheduledTimer(timeInterval: 4,target: self,selector: #selector(countup),userInfo: nil,repeats: true)
+        }else if waterAmount > 800 && waterAmount <= 1000{
+            timer = Timer.scheduledTimer(timeInterval: 5,target: self,selector: #selector(countup),userInfo: nil,repeats: true)
+        }else if waterAmount > 1000 && waterAmount <= 1200{
+            timer = Timer.scheduledTimer(timeInterval: 6,target: self,selector: #selector(countup),userInfo: nil,repeats: true)
+        }else{
+            print("水量に不備がある")
+        }
     }
 
     @objc func countup() {
