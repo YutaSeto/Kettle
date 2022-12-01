3cb44fce49f05aa27be5226790117aadb9b4d3f4
Yuta Seto <setoon47@yahoo.co.jp>
2022-12-01 23:42:25 +0900

    タイマー機能の追加

--- Kettle2.playground/Contents.swift
+++ Kettle2.playground/Contents.swift
@@ -1,8 +1,34 @@
-
 import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 //水温の変数設定 0<x<=100
 //水量の変数設定 0<y<=1200
 //スイッチを入れると時間によって温度が上がる
 //水温が100度になったらタイマーが止まる
 //水量によってかかる時間が変わる。
+
+class Alarm {
+    var timer: Timer?
+    var count: Int = 0
+    var limit: Int = 5
+    
+    func start() {
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(countup),
+            userInfo: nil,
+            repeats: true )
+    }
+
+    @objc func countup() {
+        count += 1
+        print("カウントは\(count)です")
+        if limit <= count {
+            print("ジリリリリ！(カウントをストップします)")
+            timer?.invalidate()
+        }
+    }
+}
